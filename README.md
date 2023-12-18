a9749c53e38672be5e19f44950ede7c192f02740
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-12-18 16:57:45 -0500

    Can live stream camera

---
+++ README.md
@@ -0,0 +1,17 @@
+# Chess-Robot
+
+A chess robot powered by computer vision, machine learning, and Stockfish
+running on a Raspberry Pi. (work in progress)
+
+## Install
+
+1. Install packages
+   with `sudo apt install python3-picamera2 python3-opencv opencv-data`.
+2. Clone this repo.
+3. Create a virtual environment
+   with `python -m venv --system-site-packages venv`.
+4. Install dependencies in [`requirements.txt`](./requirements.txt).
+
+## Run
+
+Run [`src/main.py`](./src/main.py) in the virtual environment. 
--- src/main.py
+++ src/main.py
@@ -1,5 +1,36 @@
 import logging
+from time import sleep
+
+import cv2
+# noinspection PyUnresolvedReferences
+from libcamera import Transform
+# noinspection PyUnresolvedReferences
+from picamera2 import Picamera2
 
 from utils.logger import create_logger
 
 logger = create_logger(name=__name__, level=logging.DEBUG)
+
+logger.debug("Starting camera")
+camera = Picamera2()
+config = camera.create_video_configuration(
+    raw={"size": (1640, 1232)},
+    main={"size": (640, 480)},
+    transform=Transform(hflip=True, vflip=True),
+)
+camera.configure(config)
+camera.start()
+sleep(1)
+
+logger.debug("Starting captures")
+while True:
+    rgb_image = camera.capture_array()
+
+    bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+
+    cv2.imshow("live", bgr_image)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cv2.destroyAllWindows()
